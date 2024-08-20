5db32d5d0cbad14ee0365ba7aaf1cc0fc80632e1
vaishnavishahakar <vaishnavishahakar1@gmail.com>
2024-08-20 14:56:16 +0530

    render the router provider

--- src/index.js
+++ src/index.js
@@ -1,10 +1,27 @@
 import ReactDOM from 'react-dom/client';
 import './index.css';
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 
 import Home from './views/Home/Home';
+import About from "./views/About/About";
+import Contact from "./views/Contact/Contact";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-    <Home/>
-);
+
+root.render(<RouterProvider router={router} />);
+
+const router = createBrowserRouter([
+    {
+      path: "/",
+      element: <Home />,
+    },
+    {
+      path: "/about",
+      element: <About />,
+    },
+    {
+      path: "/contact",
+      element: <Contact />,
+    },
+  ]);
 
